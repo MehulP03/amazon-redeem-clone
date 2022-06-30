5c90cd8a8c32300e7956900828f959865270bee6
Mehul Patangya <57245913+MehulP03@users.noreply.github.com>
2022-07-01 00:07:04 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Footer from './components/Footer';
 import HelpCustomerService from './components/HelpCustomerService';
 import Navbar from './components/Navbar';
+import NavbarMini from './components/NavbarMini';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <NavbarMini/>
       <HelpCustomerService/>
       <Footer/>
     </div>
